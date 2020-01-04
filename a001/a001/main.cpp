cb9bcaa42a8dc86731d71f53e460c761cb71e956
williamlin910403 <williamlin910403@gmail.com>
2020-01-04 17:54:07 +0800

    done

--- a001/a001/main.cpp
+++ a001/a001/main.cpp
@@ -4,12 +4,11 @@
 //
 //  Created by Wei on 2020/1/4.
 //  Copyright © 2020 Wei. All rights reserved.
-//
-
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string a ;
+    cin >> a;
+    cout << "hello, "<<a<<endl ;
     return 0;
 }
