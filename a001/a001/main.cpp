7df56b4cb68c75a4c8a717a21cb3e94de8b60e90
williamlin910403 <williamlin910403@gmail.com>
2020-01-04 17:32:09 +0800

    empty

---
+++ a001/a001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a001
+//
+//  Created by Wei on 2020/1/4.
+//  Copyright © 2020 Wei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
