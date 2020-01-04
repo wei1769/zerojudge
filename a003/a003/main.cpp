3c5eda99ffec63cb94d9ac16fd6a58e4fa6485cf
williamlin910403 <williamlin910403@gmail.com>
2020-01-04 20:14:10 +0800

    a003 done

--- a003/a003/main.cpp
+++ a003/a003/main.cpp
@@ -6,10 +6,22 @@
 //  Copyright © 2020 Wei. All rights reserved.
 //
 
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
+    int m ,d ;
+    cin >> m >> d ;
+    int s = (m*2+d)%3;
+    if (s == 0) {
+        cout << "普通\n";
+    }
+    else if (s == 1){
+        cout << "吉\n" ;
+    }
+    else if (s == 2){
+        cout << "大吉\n";
+    }
     return 0;
 }
