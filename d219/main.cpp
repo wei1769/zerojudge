46a2813a150e9d327394630bca336f57d91acafc
Wei <williamlin910403@gmail.com>
2020-01-20 15:44:23 +0800

    d219 done finally

--- d219/main.cpp
+++ d219/main.cpp
@@ -1,10 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std ;
 int main(){
-	int B ,P,M,
+	int B ,P,M;
 	while(cin >>B>>P>>M)
 	{
-
+	    B%=M;
+	    int bi = B;
+	    int x = 1;
+	    while(P)
+        {
+            if(P%2 == 1)
+            {
+                x= (x*(P%2)*bi)%M;
+            }
+            bi = (bi*bi)%M;
+            P/=2;
+        }
+            cout<<x<<endl;
 
 
 
