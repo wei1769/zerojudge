b5c7ce6c4a4e2b60e773ffd42e084b4bdbac09e8
Wei <williamlin910403@gmail.com>
2020-01-20 11:13:20 +0800

    mod d219

--- d219/main.cpp
+++ d219/main.cpp
@@ -1,22 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std ;
 int main(){
-	int B ,P,M,i,x;
+	int B ,P,M,i,x = 1;
 	while(cin >>B>>P>>M)
-	{	
+	{
 		int p = P;
 		int co = 0;
 		long long bi[50] ;
 		int p2[50];
 		for ( i= 1 ; i< P;i*=B)
-		{	
-			p2[co]= p%B;
-			p = p/B
+		{
 			bi[co]= pow(B,i);
+			cout <<"bi "<< bi[co]<<endl;
 			co++;
 		}
+		co = 0 ;
+		for (int r = 50 ; r >= 0 ; r ++ )
+		{       if(pow(B,i)<P){
+
+                p2[r]=
+		}
+        }
+
 		for ( int k = 0 ;k<50;k++)
 		{
-			x =x* p2[k]*(bi[k]%M)
+			x =x*p2[k]*(bi[k]%M);
+			cout <<  "x   "<< x <<endl ;
+		}
 
-}
+
+}}
