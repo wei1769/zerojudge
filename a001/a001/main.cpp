40bee46d2d4deed98776097a343e982724508851
williamlin910403 <williamlin910403@gmail.com>
2020-02-17 18:11:09 +0800

    remove useless file

--- a001/a001/main.cpp
+++ a001/a001/main.cpp
@@ -4,9 +4,12 @@
 //
 //  Created by Wei on 2020/1/4.
 //  Copyright © 2020 Wei. All rights reserved.
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main(int argc, const char * argv[]) {
+    cout.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     string a ;
     cin >> a;
     cout << "hello, "<<a<<endl ;
