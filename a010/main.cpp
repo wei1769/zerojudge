ee1125f71b022d104d2962794780cc8cd7842a91
williamlin910403 <williamlin910403@gmail.com>
2020-09-15 22:42:26 +0800

    add a010

---
+++ a010/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() 
+{
+    vector<long > time(1000010, 0);
+    long  x;
+    cin >> x;
+    long  y = x ;
+    for (long i = 2; i <= x; i = i + 1)
+    {
+        if (i%2 == 0 && i != 2)
+        i++;
+        if(x%i == 0)
+        {
+            time[i]++;
+            x = x/i ;
+            
+            i = i - 1;
+        }
+        if (x == 1)
+        {
+            break;
+        }
+
+    }
+    long  k = 0;
+    for (long i = 0; i <=y ; i++)
+    {
+        
+        if(time[i] == 1 )
+        {
+            
+            if(k >0 )
+            {
+                cout<< "* "<<i << " ";
+            }
+            else
+            {
+                cout << i <<" ";
+            }
+            k++;
+        }
+        else if (time[i] > 1)
+        {
+           if(k >0 )
+            {
+                cout<< "* "<<i <<"^"<< time[i] << " ";
+            }
+            else
+            {
+                cout << i <<"^"<< time[i] <<" " ;
+            } 
+            k++;
+        }
+        
+        
+    }
+    
+    
+
+}
